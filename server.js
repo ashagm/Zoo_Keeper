d5b2aa5d65d70a39b1d379c23df4b077e52653eb
ashagm <ashaagm@gmail.com>
2021-07-19 16:57:47 -0500

    push changes and add heroku

--- server.js
+++ server.js
@@ -2,8 +2,9 @@ const express = require('express');
 const { animals } = require('./data/animals');
 
  // instantiate the server
- //
+const PORT = process.env.PORT || 3001;
 const app = express();
+
 
  //create routes that the front-end can request data from
 
@@ -70,7 +71,7 @@ app.get('/api/animals', (req, res) => {
 
 // to make our server listen
 
-app.listen(3001, () => {
+app.listen(PORT, () => {
 	console.log("API server now on port 3001!");
 });
 
